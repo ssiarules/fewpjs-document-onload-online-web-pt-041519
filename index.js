9e085ea3b534e52a9749a9e679fd8a77a12c6e14
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-07 00:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document.getElementById('text').innerText = "This is really cool!"
 });
 
 console.log(
